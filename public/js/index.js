9a74753f43a6b4883891e98566a357f3ba9ae631
yash <yashojha19@gmail.com>
2018-05-05 01:58:21 +0530

    styling node chat app

--- public/js/index.js
+++ public/js/index.js
@@ -18,13 +18,16 @@ socket.on('newMessage',function(message){
     jQuery('#messages').append(li)
 })
 
+var messageTextBox = jQuery('[name=message]')
+
 jQuery('#message-form').on('submit',function(e){
     e.preventDefault();
 
     socket.emit('createMessage',{
         from:'User',
-        text:jQuery('[name=message]').val()
+        text:messageTextBox.val()
     },function(){
+        messageTextBox.val('')
         //acknowledge function.
     })
 });
@@ -40,11 +43,16 @@ socket.on('newLocationMessage',function(message){
 })
 
 var locationButton = jQuery('#send-location');
+
 locationButton.on('click', function(){
     if(!navigator.geolocation){
         return alert('Geolocation not supported by your browser.')
     }
+
+    locationButton.attr('disabled','disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function(position){
+        locationButton.removeAttr('disabled').text('Send location');
         console.log("position:",position);
             socket.emit('createLocationMessage',{
                     latitude: position.coords.latitude,
@@ -53,6 +61,7 @@ locationButton.on('click', function(){
                 console.log("Status:",fromServer)
             })
     },function(){
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     })
 })
