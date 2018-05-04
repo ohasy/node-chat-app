b6cb24ace11bd1ef95e77fb08431c213d6700181
yash <yashojha19@gmail.com>
2018-05-05 00:10:49 +0530

    add form for messages and show incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -11,6 +11,20 @@ socket.on('disconnect',function(){
     console.log('diconnected to server')
 })
 
-socket.on('newMessage',function(newMsg){
-    console.log('New msg',newMsg);
+socket.on('newMessage',function(message){
+    console.log('New msg',message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}:${message.text}`)
+    jQuery('#messages').append(li)
 })
+
+jQuery('#message-form').on('submit',function(e){
+    e.preventDefault();
+
+    socket.emit('createMessage',{
+        from:'User',
+        text:jQuery('[name=message]').val()
+    },function(){
+        //acknowledge function.
+    })
+});
--- server/server.js
+++ server/server.js
@@ -32,15 +32,11 @@ io.on('connection',(socket)=>{
     socket.emit('newMessage',generateMessage("Admin","Welcome to the chat app"))
     socket.broadcast.emit('newMessage',generateMessage("Admin","new user has joined."))
 
-    socket.on('createMessage',(message)=>{
+    socket.on('createMessage',(message,callback)=>{
         console.log('createMessage',message);
-
-        // io.emit('newMessage',{
-        //     from:msg.from,
-        //     text:msg.text,
-        //     createdAt: new Date().getTime()
-        // })
-        socket.broadcast.emit('newMessage',generateMessage(message.from,message.text))
+        io.emit('newMessage',generateMessage(message.from,message.text))
+        callback('This is from the server.')
+        // socket.broadcast.emit('newMessage',generateMessage(message.from,message.text))
     })
     socket.on('disconnect',()=>{
         console.log('Client Disconnected')
