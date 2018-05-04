2589a934e55027470c90fd5989064c9c1cd17ac1
yash <yashojha19@gmail.com>
2018-05-05 03:45:47 +0530

    scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,23 @@
 var socket = io();
+
+function scrollToBottom(){
+    //Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child')
+    //Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+       messages.scrollTop(scrollHeight);
+
+    }
+
+}
+
 socket.on('connect',function(){
     console.log('connected to server')
 
@@ -21,7 +40,7 @@ socket.on('newMessage',function(message){
     });
 
     jQuery('#messages').append(html);
-  
+    scrollToBottom();
     // console.log('New msg',message);
     // var li = jQuery('<li></li>');
     // li.text(`${message.from} ${formattedTime} :${message.text}`)
@@ -52,6 +71,7 @@ socket.on('newLocationMessage',function(message){
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
     // var formattedTime = moment(message.createdAt).format('h:mm a')//h:mm a MMM Do YYYY
     // var li = jQuery('<li></li>')
     // var a = jQuery('<a target="_blank">My Current Location</a>');
